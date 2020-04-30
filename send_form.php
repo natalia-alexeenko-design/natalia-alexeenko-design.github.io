88eba77883be3b2471b83753547df914350ab337
natalia-alexeenko-design <64535846+natalia-alexeenko-design@users.noreply.github.com>
2020-04-30 07:07:13 +0300

    rename

--- send_form.php
+++ send_form.php
@@ -1,7 +1,86 @@
-$input-text= $_POST['input-text'];
-$input-email= $_POST['input-email'];
-$emailTo = 'worms32@yandex.ru.ru'; //Сюда введите Ваш email
-$body = "$input-text \n\n$input-email ;
-$headers = "Content-Type: text/plain; charset=utf-8\r\n".'From: MySite <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $emailTo;
-mail($emailTo, $input-text, $body, $headers);
-$emailSent = true;
+<? 
+// ----------------------------конфигурация-------------------------- // 
+ 
+$adminemail="worms32@yandex.ru";  // e-mail админа 
+ 
+ 
+$date=date("d.m.y"); // число.месяц.год 
+ 
+$time=date("H:i"); // часы:минуты:секунды 
+ 
+$backurl="https://natalia-alexeenko-design.github.io/";  // На какую страничку переходит после отправки письма 
+ 
+//---------------------------------------------------------------------- // 
+ 
+  
+ 
+// Принимаем данные с формы 
+ 
+$email=$_POST['email']; 
+ 
+$text=$_POST['text']; 
+ 
+ 
+// Проверяем валидность e-mail 
+ 
+if (!preg_match("|^([a-z0-9_\.\-]{1,20})@([a-z0-9\.\-]{1,20})\.([a-z]{2,4})|is", 
+strtolower($email))) 
+ 
+ { 
+ 
+  echo 
+"<center>Вернитесь <a 
+href='javascript:history.back(1)'><B>назад</B></a>. Вы 
+указали неверные данные!"; 
+ 
+  } 
+ 
+ else { 
+  
+$text=" 
+ 
+<p>E-mail: $email</p> 
+ 
+ 
+<p>Сообщение: $text</p> 
+ 
+ 
+"; 
+ 
+  
+ 
+ // Отправляем письмо админу  
+ 
+mail("$adminemail", "$date $time Сообщение 
+от $email", "$msg"); 
+ 
+  
+ 
+// Сохраняем в базу данных 
+ 
+$f = fopen("message.txt", "a+"); 
+ 
+fwrite($f," \n $date $time Сообщение от $email"); 
+ 
+fwrite($f,"\n $text "); 
+ 
+fwrite($f,"\n ---------------"); 
+ 
+fclose($f); 
+ 
+  
+ 
+// Выводим сообщение пользователю 
+ 
+print "<script language='Javascript'><!-- 
+function reload() {location = \"$backurl\"}; setTimeout('reload()', 6000); 
+//--></script> 
+ 
+$text 
+ 
+<p>Сообщение отправлено! Подождите, сейчас вы будете перенаправлены на главную страницу...</p>";  
+exit; 
+ 
+ } 
+ 
+?>
